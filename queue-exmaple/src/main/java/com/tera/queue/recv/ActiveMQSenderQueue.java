d4b81727b8e82dbfb214eb7652f297b9a8640ed8
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2019-08-18 16:15:23 +0900

    ActiveMQクラスの利用

---
+++ queue-exmaple/src/main/java/com/tera/queue/recv/ActiveMQSenderQueue.java
@@ -0,0 +1,49 @@
+package com.tera.queue.recv;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ActiveMQSenderQueue {
+    public static void main(String[] args) {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Destination destination = session.createQueue("TEST.FOO");
+
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            List<Map<String, Integer>> lists = IntStream.range(0, 5).mapToObj(it -> {
+                return new HashMap<String, Integer>() {{
+                    put("key" + it, it);
+                }};
+            }).collect(Collectors.toList());
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(lists);
+            TextMessage message = session.createTextMessage(json);
+
+            System.out.println("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
+            producer.send(message);
+
+            session.close();
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ topic-exmaple/src/main/java/com/tera/topic/recv/ActiveMQReceiverTopic.java
@@ -0,0 +1,51 @@
+package com.tera.topic.recv;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class ActiveMQReceiverTopic implements ExceptionListener {
+    public static void main(String[] args) {
+        ActiveMQReceiverTopic topic = new ActiveMQReceiverTopic();
+        topic.run();
+    }
+
+    public void run() {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            connection.setExceptionListener(this);
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Destination destination = session.createQueue("TEST.FOO");
+
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            Message message = consumer.receive(1000);
+
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = textMessage.getText();
+                System.out.println("Received: " + text);
+            } else {
+                System.out.println("Received: " + message);
+            }
+
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public synchronized void onException(JMSException ex) {
+        System.out.println("JMS Exception occured.  Shutting down client.");
+    }
+}
